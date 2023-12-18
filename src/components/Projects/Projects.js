790aab884b2a2f7c57403d265c701bcaff41235b
Ricky Primayuda Putra <113336156+rickyprimay@users.noreply.github.com>
2023-12-18 12:21:24 +0700

    add lms project

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -5,6 +5,7 @@ import Particle from "../Particle";
 import hitech from "../../Assets/Projects/hitech.jpeg";
 import ppko from "../../Assets/Projects/ppko.png";
 import codejam from "../../Assets/Projects/cja.png";
+import lms from "../../Assets/Projects/lms.png";
 
 function Projects() {
   return (
@@ -28,6 +29,16 @@ function Projects() {
               description="Im Building Website to joining Hi-Technology 2023 Software Fair, TechStack i use is Svelte Kit, Supabase, PostgreSQL. For Face Recognition i use API from face-api.js and geolocation i use mozila WEB API"
               ghLink = "https://github.com/RickyPrima30/presensi-facerecognition-geolocation"
               demoLink = "https://presensi-mhs.netlify.app/"
+            />
+          </Col>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={lms}
+              isBlog={false}
+              title="Learning Management System"
+              description="Developed a learning management system website for a mini project organized by the organization. Integrated OAuth, email verification link, and email verification code. The website serves as a learning platform, enabling users to engage with structured learning videos segmented per material. Utilized the following TechStack: Next.js, PostgreSQL, Supabase, Prisma, and Shadcnui."
+              ghLink = "https://github.com/rickyprimay/lms"
+              demoLink = "https://rickieslearn.vercel.app/"
             />
           </Col>
           <Col md={4} className="project-card">
